c6c602e886d90cc77ee283dfd0b49df5f84f261c
Jakub Kaczmarski <61906394+jakubkaczmarski@users.noreply.github.com>
2021-01-21 15:49:50 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+bool isPalindrome(const std::string &text)
+{
+    for (int i = 0; i < text.length(); ++i)
+    {
+
+        if (tolower(text[i]) != tolower(text[text.length() - 1 - i]))
+        {
+
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    std::string usrInput{};
+    std::cout << "Wprowadź słowo:"<< "\n";
+              
+    std::cin >> usrInput;
+
+    if (isPalindrome(usrInput))
+    {
+        std::cout << "Jest palindromem" << "\n";
+                  
+    }
+    else
+    {
+        std::cout << "Nie jest palindromem" << "\n";
+                  
+    }
+    return 0;
+}
